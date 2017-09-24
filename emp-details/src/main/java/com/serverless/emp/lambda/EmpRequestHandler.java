a641a5def0b6a871d8089ea3d92974cebc43b19e
pallavi2146 <pallavi2146@gmail.com>
2017-09-25 02:39:05 +0530

    [emp-details] : updated EmpRequestHandler

--- emp-details/src/main/java/com/serverless/emp/lambda/EmpRequestHandler.java
+++ emp-details/src/main/java/com/serverless/emp/lambda/EmpRequestHandler.java
@@ -1,8 +1,12 @@
 package com.serverless.emp.lambda;
 
+import com.amazonaws.Response;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.serverless.emp.Bean.Employee;
+import com.serverless.emp.rds.EmpDAO;
 import org.json.simple.parser.JSONParser;
 
 import java.io.*;
@@ -10,9 +14,9 @@ import java.io.*;
 /**
  * Created by pallavi on 21/9/17.
  */
-public class EmpRequestHandler implements RequestStreamHandler {
+public class EmpRequestHandler implements RequestHandler<Employee, String> {
 
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    public String handleRequest(Employee emp, Context context){
 
         JSONParser parser = new JSONParser();
 
@@ -20,7 +24,11 @@ public class EmpRequestHandler implements RequestStreamHandler {
         LambdaLogger logger = context.getLogger();
         logger.log("Loading Java Lambda handler of EmpRequestHandler");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        EmpDAO empDAO = new EmpDAO();
+        empDAO.addEmpDetails(emp);
+
+
+        return  "Employee details added into the DB.";
 
 
     }
